3498d86409ab4e3b99082347b8e5e7f6a2120506
Henry Elano Klitzke <henry.elano@gmail.com>
2023-05-02 18:02:59 -0300

    comit

--- src/Controller/ControllerContato.php
+++ src/Controller/ControllerContato.php
@@ -3,36 +3,112 @@ namespace App\Controller;
 
 use App\Entity\Contato;
 use App\Form\ContatoType;
-use App\Repository\PessoaRepository;
+use App\Repository\ContatoRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ControllerContato extends AbstractController {
 
-    public function index(EntityManagerInterface $oEntityManager, PessoaRepository $oRepositorioPessoa) {
-        $oPessoa = $oRepositorioPessoa->find(1);
-        $oContato = new Contato();
-        $oContato->setTipo(1);
-        $oContato->setDescricao('lucas.silva@gmail.com');
-        $oContato->setPessoa($oPessoa);
+    private $Form;
 
-        $sMsg = '';
-        try{
-            $oEntityManager->persist($oContato);
-            $oEntityManager->flush(); 
-            $sMsg = 'Contato inserido com sucesso!';
-        }catch(Exception $e) {
-            $sMsg = 'Erro ao Inserir o contato'. ' '. $e;
-        }
-        return new Response('<h1>'.$sMsg.'</h1>');
+    public function consultaContatos(ContatoRepository $oRepositorioContato) {
+        $aDados['contatos'] = $oRepositorioContato->findAll();
+        $aDados['titulo']  = 'Gerenciar Contatos';
+        
+        return $this->render('contato/consulta.html.twig', $aDados);
     }
 
-    public function salvar() {
-        $aDados['titulo'] = 'Adicionar um Novo Contato';
-        $aDados['form']   = $this->createForm(ContatoType::class);
-        return $this->render('Contato/form.html.twig', $aDados);
+    public function inserir(Request $oRequest, EntityManagerInterface $oEntity) {
+        $sMsg = '';
+        $oContato = new Contato();
+        if($this->executaInclusao($oContato, $oRequest, $oEntity)) {
+            $sMsg = 'Contato inserido com sucesso!';
+        }
+        $aDados['titulo'] = 'Adicionar um novo contato';
+        $aDados['form']   = $this->getForm($oContato, $oRequest);
+        $aDados['msg']    = $sMsg;
+        return $this->render('Pessoa/form.html.twig', $aDados);
+    }
+
+    /**
+     * Rota para Editar os dados da pessoa.
+     * @param Integer $id
+     * @param \Symfony\Component\HttpFoundation\Request $oRequest
+     * @param \Doctrine\ORM\EntityManagerInterface      $oEntity
+     * @param \App\Repository\ContatoRepository         $oRepositorioContato
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function editar($id, Request $oRequest, EntityManagerInterface $oEntity, ContatoRepository $oRepositorioContato) { 
+        $sMsg = '';
+        $oContato = $oRepositorioContato->find($id);
+
+        if($this->executaAlteracao($oContato, $oRequest, $oEntity)) {
+            $sMsg = 'Pessoa editada com sucesso!';
+        }
+
+        $aDados['titulo'] = 'Adicionar novo contato';
+        $aDados['form']   = $this->getForm($oContato, $oRequest);
+        $aDados['msg']    = $sMsg;
+        return $this->render('Pessoa/form.html.twig', $aDados);
+    }
+
+    /**
+     * Rota para exclusão da pessoa.
+     * @param Integer $id
+     * @param \App\Repository\ContatoRepository $oRepositorioContato
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function excluir($id, EntityManagerInterface $oEntity, ContatoRepository $oRepositorioContato) {
+        $oEntity->remove($oRepositorioContato->find($id));
+        $oEntity->flush();
+        return $this->redirectToRoute('consultaContatos');
+    }
+
+    /**
+     * Executa a inclusão de um novo contato.
+     */
+    private function executaInclusao($oContato, $oRequest, $oEntity) {
+        $bInseriu = false;
+        if($this->getForm($oContato, $oRequest)->isSubmitted() && $this->Form->isValid()) {
+            $oEntity->persist($oContato);
+            $oEntity->flush();
+            $bInseriu = true;
+        }
+        return $bInseriu;
+    }
+
+    /**
+     * Executa a alteração do contato.
+     */
+    private function executaAlteracao($oContato, $oRequest, $oEntity) {
+        $bAlterou = false;
+        if($this->getForm($oContato, $oRequest)->isSubmitted() && $this->Form->isValid()) {
+            $oEntity->flush();
+            $bAlterou = true;
+        }
+        return $bAlterou;
+    }
+
+    /**
+     * Retorna o formulário.
+     * @param Contato
+     * @param Symfony\Component\HttpFoundation\Request
+     * @return \Symfony\Component\Form\FormInterface
+     */
+    private function getForm($oContato, $oRequest) {
+        return $this->Form ?: $this->createFormPessoa($oContato, $oRequest);
+    }
+
+    /**
+     * Cria o formulário de Contato.
+     */
+    private function createFormPessoa($oContato, $oRequest) {
+        $this->Form = $this->createForm(ContatoType::class, $oContato);
+        $this->Form->handleRequest($oRequest);
+        return $this->Form;
     }
 
 
--- src/Form/ContatoType.php
+++ src/Form/ContatoType.php
@@ -5,7 +5,7 @@ namespace App\Form;
 use App\Entity\Pessoa;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,9 +13,11 @@ use Symfony\Component\Form\FormBuilderInterface;
 class ContatoType extends AbstractType {
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder->add('tipo'     , CheckboxType::class, ['label' => 'Tipo do Contato: '])
+        $builder->add('tipo',ChoiceType::class, [ 'choices' => ['Telefone'  => '0', 'Email' => '1'],
+                                                  'label'   => 'Tipo de contato: '
+                                                ])
                 ->add('descricao', TextType::class    , ['label' => 'Descrição do Contato: '])
-                ->add('pessoa_id', EntityType::class  , ['label' => 'Pessoa: ', 'class' => Pessoa::class, 'choice_label' => 'nome'])
+                ->add('Pessoa', EntityType::class  , ['label' => 'Pessoa: ', 'class' => Pessoa::class, 'choice_label' => 'nome'])
                 ->add('Salvar', SubmitType::class);
     }
 
